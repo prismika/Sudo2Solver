bda859a112087cb2e81849cd7e7157db193b7a24
Brantley Vose <brantmv@iastate.edu>
2017-02-15 23:45:24 -0600

    Create square, untested. Add exception class.

--- Sudoku/src/solver/Square.java
+++ Sudoku/src/solver/Square.java
@@ -1,5 +1,52 @@
 package solver;
 
-public class Square {
+import java.util.BitSet;
 
+public class Square {
+	private BitSet poss;
+	Square(int boardSize){
+		poss = new BitSet(boardSize*boardSize);
+		poss.set(0,boardSize*boardSize-1);
+	}
+	Square(Square original){
+		BitSet newPoss = new BitSet(poss.cardinality());
+		for(int i = 0; i < poss.size(); i++){
+			newPoss.set(i,poss.get(i));
+		}
+		this.poss = newPoss;
+	}
+	public void removePossibility(int newVal){
+		poss.clear(newVal-1);
+		if(poss.cardinality()==0){
+			throw new SquareContradictionException("Square contradicted upon removal of: " + newVal);
+		}
+	}
+	public int[] getPossibilities(){
+		int[] reply = new int[poss.cardinality()];
+		int counter = 0;
+		for(int i = 0; i < poss.size(); i++){
+			if(poss.get(i))
+				reply[counter++] = i+1;
+		}
+		return reply;
+	}
+	public boolean checkPossibility(int check){
+		return poss.get(check-1);
+	}
+	public boolean hasValue(){
+		return poss.cardinality()==1;
+	}
+	public int getValue(){
+		if(hasValue()){
+			for(int i = 0; i < poss.size(); i++){
+				if(poss.get(i)){
+					return i+1;
+				}
+			}
+		}
+		return 0;
+	}
+	public Square clone(){
+		return new Square(this);
+	}
 }
---
+++ Sudoku/src/solver/SquareContradictionException.java
@@ -0,0 +1,8 @@
+package solver;
+
+public class SquareContradictionException extends RuntimeException{
+	private static final long serialVersionUID = 1420939194642509721L;
+	public SquareContradictionException(String s){
+		super(s);
+	}
+}
