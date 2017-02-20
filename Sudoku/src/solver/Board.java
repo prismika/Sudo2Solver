6adf72c363e4bb0b9b480dcf55a008695f369f0a
Brantley Vose <brantmv@iastate.edu>
2017-02-20 17:01:09 -0600

    Awesome spaghetti (Board initial commit)

--- Sudoku/src/solver/Board.java
+++ Sudoku/src/solver/Board.java
@@ -1,5 +1,26 @@
 package solver;
 
 public class Board {
-
+	private Square[][] square;
+	private int size;
+	public Board(int size){
+		for(Square[] col : square){
+			for(Square s : col){
+				s = new Square(size);
+			}
+		}
+		this.size = size;
+	}
+	public Board(int size, Board b){
+		for(int i = 0; i < size*size; i++){
+			for(int j = 0; j < size*size; j++){
+				square[i][j] = new Square(b.getSquare(i, j));
+			}
+		}
+		this.size = size;
+	}
+	
+	public Square getSquare(int row, int col){return square[row][col];}
+	public int getSize(){return size;}
+	public int getHeight(){return size*size;}
 }
